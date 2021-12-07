9eec13e9c9066fdf6bd218b977ba3dcc869de43b
Risang Baskoro <risangbaskoro@gmail.com>
2021-12-07 14:36:40 +0700

    add footer

---
+++ footer.php
@@ -0,0 +1,20 @@
+<footer class="text-center footer bg-light py-5">
+
+    <a href="index.php">
+        <img class="my-3" src="img/VaksinGO-logo.png" alt="brand">
+    </a>
+
+    <div class="mb-3">
+        <a href="">Terms of Service</a>
+        |
+        <a href="">Privacy policy</a>
+    </div>
+
+    <div class="">
+        © 
+        <?php
+        echo date("Y");
+        ?>
+        | VaksinGO
+    </div>
+</footer>
