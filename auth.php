3afa1f2faecbe87986403f356a364a9a959240c4
Risang Baskoro <risangbaskoro@gmail.com>
2021-12-07 12:12:22 +0700

    auth include config.php and default timezone

--- auth.php
+++ auth.php
@@ -1,5 +1,7 @@
 <?php
 
+date_default_timezone_set("Asia/Jakarta");
+include('config.php');
 session_start();
 
 if (!isset($_SESSION["user"]) && $_SERVER['REQUEST_URI'] != "/index.php") header("Location: login.php");
