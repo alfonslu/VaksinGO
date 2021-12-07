5b7dc555b1db5e5bb7b6b47dcdea226cd5befed3
Risang Baskoro <risangbaskoro@gmail.com>
2021-12-07 12:22:39 +0700

    update sql and images

--- vaksingo.sql
+++ vaksingo.sql
@@ -2,10 +2,10 @@
 -- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Nov 04, 2021 at 07:28 PM
--- Server version: 10.4.19-MariaDB
--- PHP Version: 8.0.6
+-- Host: localhost:8889
+-- Generation Time: Dec 06, 2021 at 06:55 PM
+-- Server version: 5.7.34
+-- PHP Version: 7.4.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,21 @@ SET time_zone = "+00:00";
 --
 -- Database: `vaksingo`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hospital`
+--
+
+CREATE TABLE `hospital` (
+  `id` mediumint(9) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `address` text NOT NULL,
+  `latitude` text NOT NULL,
+  `longitude` text NOT NULL,
+  `vaccine_quota` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -41,13 +56,61 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `email`, `password`, `name`, `no_telp`, `username`) VALUES
-(2, 'irsyadabqori@gmail.com', '$2y$10$h9L.nXHCnJy0yh84RBKSJ.Lv1qJSB67lDv1tguudkhi4jCvO11f.K', 'Irsyad Abqori Fikri', '085903629847', 'aleirsyad'),
-(3, 'salmaalleak@gmail.com', '$2y$10$KJJLguHlMfZcVujHhNyZmuI9TZA9nZuD7IQ/HB8kSCjK/1amoc/Mm', 'Salma Allea Kirana', '08371289467913', 'salmaallea'),
-(4, 'okin123@gmail.com', '$2y$10$JIO8GfZ6yNsik/BF/YEp3uE877x4V.aoK/INKMXqMXgsaA0wPSDXK', 'okin', '08778585686', 'okin123');
+(5, 'harper@gmail.com', '$2y$10$8gUW3E62JC4f1Rc9wpuTaOLq6L5TpLUhfYpYTuqY3Ua6JfBGoavnC', 'Hario Perdana', '081283838383', 'harper');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vaccine_schedule`
+--
+
+CREATE TABLE `vaccine_schedule` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `nik` varchar(16) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `vaccine_id` tinyint(4) NOT NULL,
+  `hospital_id` smallint(4) NOT NULL,
+  `date_created` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `vaccine_schedule`
+--
+
+INSERT INTO `vaccine_schedule` (`id`, `user_id`, `nik`, `address`, `vaccine_id`, `hospital_id`, `date_created`) VALUES
+(1, 5, '3275101908000384', 'Jl. Licin No.30 Semarang.', 1, 1, '2021-12-07');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vaccine_type`
+--
+
+CREATE TABLE `vaccine_type` (
+  `id` smallint(6) NOT NULL,
+  `vaccine_name` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `vaccine_type`
+--
+
+INSERT INTO `vaccine_type` (`id`, `vaccine_name`) VALUES
+(1, 'Sinovac'),
+(2, 'Pfizer'),
+(3, 'AstraZenica'),
+(4, 'Sinopharm');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `hospital`
+--
+ALTER TABLE `hospital`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `user`
@@ -56,14 +119,55 @@ ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `vaccine_schedule`
+--
+ALTER TABLE `vaccine_schedule`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`) USING BTREE;
+
+--
+-- Indexes for table `vaccine_type`
+--
+ALTER TABLE `vaccine_type`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `hospital`
+--
+ALTER TABLE `hospital`
+  MODIFY `id` mediumint(9) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `vaccine_schedule`
+--
+ALTER TABLE `vaccine_schedule`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `vaccine_type`
+--
+ALTER TABLE `vaccine_type`
+  MODIFY `id` smallint(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `vaccine_schedule`
+--
+ALTER TABLE `vaccine_schedule`
+  ADD CONSTRAINT `vaccine_schedule_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
